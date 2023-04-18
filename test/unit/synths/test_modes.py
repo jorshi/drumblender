ab4726b2489fbe7b79c6ab27ed928840d46a5994
jorshi <j.m.shier@qmul.ac.uk>
2023-04-18 13:58:13 +0100

    smaller output for modal synth test

--- test/unit/synths/test_modes.py
+++ test/unit/synths/test_modes.py
@@ -13,23 +13,24 @@ def modal_synth():
 
 def test_modal_synth_produces_correct_output_size_three_inputs(modal_synth):
     hop_size = 256
-    batch_size = 16
-    frame_length = 690
-    num_modes = 120
+    batch_size = 5
+    frame_length = 120
+    num_modes = 24
 
     amp_env = torch.rand(batch_size, num_modes, frame_length)
     freq_env = torch.rand(batch_size, num_modes, frame_length)
     phases = torch.rand(batch_size, num_modes)
 
     y = modal_synth((amp_env, freq_env, phases))
+    print(y.shape)
     assert y.shape == (batch_size, hop_size * (frame_length - 1))
 
 
 def test_modal_synth_produces_correct_output_size_two_inputs(modal_synth):
     hop_size = 256
-    batch_size = 16
+    batch_size = 4
     frame_length = 690
-    num_modes = 120
+    num_modes = 3
 
     amp_env = torch.rand(batch_size, num_modes, frame_length)
     freq_env = torch.rand(batch_size, num_modes, frame_length)
@@ -42,7 +43,7 @@ def test_modal_synth_produces_test_tone(modal_synth):
     hop_size = 256
     batch_size = 16
     frame_length = 690
-    num_modes = 120
+    num_modes = 2
     freq_tone = 1500
     sr = 48000
 
