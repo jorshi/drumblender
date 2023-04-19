150b7ff27fea0c77206f243075fcc4f632ca3bd7
jorshi <j.m.shier@qmul.ac.uk>
2023-04-19 16:07:14 +0100

    Removing old tasks

--- drumblender/tasks/__init__.py
+++ drumblender/tasks/__init__.py
@@ -1,5 +1,3 @@
-from drumblender.tasks.kicksynth import KickSynth
-from drumblender.tasks.regression import Regression
 from drumblender.tasks.drumblender import DrumBlender
 
-__all__ = ["DrumBlender", "KickSynth", "Regression"]
+__all__ = ["DrumBlender"]
