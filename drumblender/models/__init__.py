66853b072e14785e65d1ad679ff65d67dea8dcb4
Franco Caspe <francocaspe@gmail.com>
2023-08-15 17:27:58 +0100

    Removed unused modules and test routines. All models start training correctly. TFilm and GatedActivation are left in repo so not to alter the TCN class implementation.

--- drumblender/models/__init__.py
+++ drumblender/models/__init__.py
@@ -1,8 +1,4 @@
-from drumblender.models.eat import EAT
-from drumblender.models.encoders import AutoEncoder
-from drumblender.models.encoders import ModalAutoEncoder
 from drumblender.models.mlp import MLP
-from drumblender.models.tcn import KickTCN
 from drumblender.models.tcn import TCN
 
-__all__ = ["AutoEncoder", "ModalAutoEncoder", "MLP", "TCN", "KickTCN", "EAT"]
+__all__ = ["MLP", "TCN"]
